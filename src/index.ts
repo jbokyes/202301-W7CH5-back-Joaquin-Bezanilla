601f56ed5baef688b2ec89c2b7b994b0ba526370
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-04 13:23:49 +0100

    Add port and createserver. Also dbConnect.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 import createDebug from 'debug';
 import http from 'http';
+import { app } from './app';
+import { dbConnect } from './db/connect.db';
 
-const debug = createDebug('W7-CH5');
+const debug = createDebug('W7-CH5:index');
+
+const PORT = process.env.PORT || 4500;
+
+const server = http.createServer(app);
+
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('DB: ', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => server.emit('error', error));
+server.on('error', (error) => {
+  debug('Server error', error.message);
+});
+server.on('listening', () => {
+  debug('Listening in http://localhost:' + PORT);
+});
