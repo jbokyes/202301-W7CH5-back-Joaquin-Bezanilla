40c4daa8953757894ab9f6ae9493fed311295a08
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-05 12:50:18 +0100

    Add register and login routes to router

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -9,3 +9,5 @@ const controller = new UsersController(repoUsers);
 
 userRouter.get('/', controller.getAllUsers.bind(controller));
 userRouter.get('/:id', controller.getUser.bind(controller));
+userRouter.post('/register', controller.register.bind(controller));
+userRouter.post('/login', controller.login.bind(controller));
