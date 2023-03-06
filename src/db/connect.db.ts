ddd24f2c1f750e68b700d1299992bb34020d3d37
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-06 11:56:42 +0100

    Add code review with Alejandro

--- src/db/connect.db.ts
+++ src/db/connect.db.ts
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 import { config } from '../config.js';
 const { user, password, cluster, name } = config;
 
-export const dbConnect = () => {
+export const dbConnect = (env?: string) => {
+  // Opción presentada por el profesor
+  // const finalEnv = env || process.env.NODE_ENV;
+  // const finalDBName = finalEnv === 'test' ? name + '_Testing' : name;
   const uri = `mongodb+srv://${user}:${password}@${cluster}/${name}?retryWrites=true&w=majority`;
   return mongoose.connect(uri);
 };
